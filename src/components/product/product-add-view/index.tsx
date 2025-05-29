bd173c3d5374e03f7a336c06b60d2fb667c01017
hyrbi <alexey.s.litvinov@gmail.com>
2025-05-29 21:49:13 +0300

    fix: form validation;

--- src/components/product/product-add-view/index.tsx
+++ src/components/product/product-add-view/index.tsx
@@ -2,8 +2,12 @@
 
 import { LeftOutlined } from '@ant-design/icons';
 import { Button, Col, Flex, Form, Input, InputNumber, Row, Typography } from 'antd';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import React from 'react';
+
+import { addMeal } from '@/api';
+import { APP_ROUTES } from '@/const/routes';
+import { MealType } from '@/model/meal';
 
 interface IAddNewProductValues {
   productName: string;
@@ -15,11 +19,24 @@ interface IAddNewProductValues {
 }
 
 export const ProductAddView = () => {
-  const { back } = useRouter();
+  const { back, replace } = useRouter();
+  const searchParams = useSearchParams();
   const [form] = Form.useForm<IAddNewProductValues>();
 
+  const type = searchParams.get('type');
+  const date = searchParams.get('date');
+
   const onAddClick = async (values: IAddNewProductValues) => {
-    console.log(values);
+    const tgId = window?.Telegram?.WebApp?.initDataUnsafe?.user?.id;
+
+    await addMeal({
+      date,
+      type: type as MealType,
+      tgId: tgId?.toString(),
+      ...values
+    }).then(() => {
+      replace(APP_ROUTES.ANALYSIS);
+    });
   };
 
   return (
@@ -30,35 +47,85 @@ export const ProductAddView = () => {
           Добавление продукта
         </Typography.Title>
       </Flex>
-      <Form layout="vertical" variant="filled" form={form} onFinish={onAddClick}>
+
+      <Form layout="vertical" variant="filled" form={form} onFinish={onAddClick} requiredMark="optional">
         <Row gutter={[16, 0]}>
           <Col xs={24}>
-            <Form.Item required name="productName" label="Название продукта">
+            <Form.Item
+              rules={[
+                {
+                  required: true
+                }
+              ]}
+              name="productName"
+              label="Название продукта"
+            >
               <Input placeholder="Введите название продукта" />
             </Form.Item>
           </Col>
           <Col xs={24}>
-            <Form.Item required name="kcal" label="Калорийность">
+            <Form.Item
+              rules={[
+                {
+                  required: true
+                }
+              ]}
+              name="kcal"
+              label="Калорийность"
+              tooltip="на 100г"
+            >
               <InputNumber style={{ width: '100%' }} placeholder="0" />
             </Form.Item>
           </Col>
           <Col xs={8}>
-            <Form.Item required name="protein" label="Белки">
+            <Form.Item
+              rules={[
+                {
+                  required: true
+                }
+              ]}
+              name="protein"
+              label="Белки"
+            >
               <InputNumber style={{ width: '100%' }} placeholder="0" />
             </Form.Item>
           </Col>
           <Col xs={8}>
-            <Form.Item required name="fat" label="Жиры">
+            <Form.Item
+              rules={[
+                {
+                  required: true
+                }
+              ]}
+              name="fat"
+              label="Жиры"
+            >
               <InputNumber style={{ width: '100%' }} placeholder="0" />
             </Form.Item>
           </Col>
           <Col xs={8}>
-            <Form.Item required name="carbs" label="Углеводы">
+            <Form.Item
+              rules={[
+                {
+                  required: true
+                }
+              ]}
+              name="carbs"
+              label="Углеводы"
+            >
               <InputNumber style={{ width: '100%' }} placeholder="0" />
             </Form.Item>
           </Col>
           <Col xs={24}>
-            <Form.Item required name="amountGrams" label="в граммах">
+            <Form.Item
+              rules={[
+                {
+                  required: true
+                }
+              ]}
+              name="amountGrams"
+              label="В граммах"
+            >
               <InputNumber style={{ width: '100%' }} placeholder="0" />
             </Form.Item>
           </Col>
